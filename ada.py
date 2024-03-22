8154866ad9e2315e9e56cc280057a2b94c48ec95
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-03-22 13:02:07 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -4,7 +4,7 @@ def ada():
     total_1= f"{first_name.lower()} {last_name.lower()}"
     total_2= f"{first_name.title()} {last_name.title()}"
     total_3= f"{first_name.upper()} {last_name.upper()}"
-    total_4= f" {first_name.lower()} {last_name.lower()}"
+    total_4= f"\t{first_name.lower()} {last_name.lower()}"
     print(total_1)
     print(total_2)
     print(total_3)
