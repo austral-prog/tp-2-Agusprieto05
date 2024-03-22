d67d904e6c9179581102b91eaf305d49b3889c5b
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-03-22 12:48:33 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,11 +1,15 @@
 def change():
     g1= float(input("Ingrese gasto: ")) 
-    g2= float(input("Ingrese dinero recibido: "))
+    g2= float(input("Ingrese recivido "))
     c1= int(g2-g1)
     c2= (float(g2-g1))
     c3= c2-c1
     
+    print("Vuelto")
+    print(" ")
+    print("Pesos:")
     print(c1)
+    print("Centavos: "
     print(c3)
 change()
     
