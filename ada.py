a4daddb4f4cac35ec176d96202940a15f627be39
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-03-22 19:48:55 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,12 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    total_1= f"{first_name.lower()} {last_name.lower()}"
-    total_2= f"{first_name.title()} {last_name.title()}"
-    total_3= f"{first_name.upper()} {last_name.upper()}"
-    total_4= f"\t{first_name.lower()} {last_name.lower()}"
-    print(total_1)
-    print(total_2)
-    print(total_3)
-    print(total_4)
+    tot_name= first_name+" "+last_name
+    print(tot_name.lower())
+    print((tot_name.lower()).title())
+    print(tot_name.upper())
+    print("\t"+ tot_name.lower())
 
