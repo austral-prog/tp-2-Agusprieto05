91b9813b5c943aeb895aab490c47d1bb62b7c41e
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-03-26 14:52:44 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -3,7 +3,7 @@ def ada():
     last_name = "LoVeLAce"
     tot_name= first_name+" "+last_name
     print(tot_name.lower())
-    print((tot_name.lower()).title())
+    print(tot_name.title())
     print(tot_name.upper())
     print("\t"+ tot_name.lower())
 
